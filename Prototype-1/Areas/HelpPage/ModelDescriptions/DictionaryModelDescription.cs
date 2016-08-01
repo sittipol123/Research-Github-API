9037fce15f20b7bbe1c7f9aaddbae453ffdaad8d
sittipol123 <sittipol123@gmail.com>
2016-08-01 18:17:36 +0700

    First commit

---
+++ Prototype-1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Prototype_1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Prototype-1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prototype_1.WebApiApplication" Language="C#" %>
